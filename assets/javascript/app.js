3e86a3a495386028e087336f5b8f0292dc5e6a03
unknown <t.bakeer4@gmail.com>
2018-05-18 18:59:37 -0700

    adding final result screen  and end of game funcion in javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 var myInterval;
-
 var count = 10;
+
 var trivia = {
     questions: ["What type of animal is a seahorse?", "Which of the following dogs is the smallest?", "What existing bird has the largest wingspan?"],
     answers1: ["A) Crustacean", "A) Dachshund", "A) Stork"],
@@ -25,13 +25,15 @@ var trivia = {
     },
 
     checkAnswer: function (value) {
-        if (value === this.correctAnswers[this.index].number) {
+        if (value === this.correctAnswers[this.index].number) 
+        {
             console.log("correct");
             this.correct++;
             this.isCorrect = true;
             this.fillResult("correct");
         }
-        else {
+        else
+         {
             console.log("incorrect");
             this.incorrect++;
             this.isCorrect = false;
@@ -42,33 +44,51 @@ var trivia = {
 
     },
 
-    endGame:function()
-    {
-
-    },
+ 
 
     fillResult:function(value)
-    {  var correctAnswer=this.correctAnswers[this.index].value;
+    { 
+
+        var correctAnswer=this.correctAnswers[this.index].value;
+        $("#result").empty();
+        $("#imgDiv").empty();
         var imgDiv=$("<img>");
+
         if(value==="correct")
         {
             imgDiv.attr("src","assets/images/win.png");
-            $("#result").text("correct !!!");
+            $("#result").append( $("<P class='mt-2'>").text("correct !!!"));
         }
         else if(value==="incorrect")
         {
-            $("#result").text("incorrect  !!! , Correct answer was "+correctAnswer);
-       imgDiv.attr( "src","assets/images/youlose.png");
+            $("#result").append( [$("<P class='mt-2'>").text("incorrect !!!"),$("<P class='mt-2'>").text("Correct answer was "+correctAnswer)]);
+             imgDiv.attr( "src","assets/images/youlose.png");
         }
-       else{
-        $("#result").text("out of time !! ,  Correct answer was "+correctAnswer)
+       else
+        {
+        $("#result").append( [$("<P class='mt-2'>").text("out of time !!"),$("<P class='mt-2'>").text("Correct answer was "+correctAnswer)]);
         imgDiv.attr("src","assets/images/outOfTime.png");
-       }
+        }
         imgDiv.css("hieght", "320");
         imgDiv.css("width", "330");
-        $("#imgDiv").empty();
         $("#imgDiv").append(imgDiv);
        
+    },
+
+
+    fillFinalResult:function(numCorrect,numIncorrect,numUnanswered)
+    {
+        $("#result").empty();
+        $("#imgDiv").empty();
+        $("#result").append( [$("<P>").text("End Of The Game"),$("<P class='mt-2'>").text("Correct answers= "+numCorrect),$("<P class='mt-2'>").text("inCorrect answers= "+numIncorrect),$("<P class='mt-2'>").text("UnAnswered Questions= "+numUnanswered)]);
+      //  $("#result").text("correct answers = "+ this.correct + " AND incorrect answers ="+ this.incorrect +" AND un answered Questions ="+this.unAnswered);
+        var startButton=$("<button >").text("start Again");
+       
+        startButton.addClass("mt-5 mb-5 p-3 btn-lg btn-block  text-white startAgain");
+        startButton.text("start Again");
+        
+        $("#imgDiv").append(startButton);
+
     }
 
 
@@ -76,7 +96,8 @@ var trivia = {
 
 }
 
-$("#startButton").on("click", function () {
+
+$(".start").on("click", function () {
     console.log("hi");
     $(".myRow1").addClass("invisible");
     $(".myRow1").addClass("tempwidth");
@@ -87,15 +108,17 @@ $("#startButton").on("click", function () {
 
 })
 
+
 var displayTimer = function () {
     count--;
-$(".timer").text("Time Remaining = " + count);
+   $(".timer").text("Time Remaining = " + count);
     if (count === 0) {
         clearInterval(myInterval);
         trivia.unAnswered++;
         $(".myRow2").addClass("invisible");
         $(".myRow3").removeClass("invisible");
         trivia.fillResult("out");
+        
         setTimeout(goBackToQuestion,3000);
    
     }
@@ -105,15 +128,22 @@ $(".timer").text("Time Remaining = " + count);
 var goBackToQuestion=function()
 { 
     count = 10;
+    trivia.index++;
+
+    if (trivia.index === trivia.questions.length) 
+    { 
+      setTimeout(trivia.fillFinalResult(trivia.correct,trivia.incorrect,trivia.unAnswered),3000);
+      console.log("end"+trivia.correct);
+
+    }
+    else{
     $(".myRow2").removeClass("invisible");
     $(".myRow3").addClass("invisible");
     $(".myRow3").addClass("tempwidth");
    
-    trivia.index++;
-    if (trivia.index === trivia.questions.length) { trivia.index = 0; }
     trivia.fillQuestionAndAnswer();
     myInterval = setInterval(displayTimer, 1000);
-
+}
 
 }
 $(".mybutton").on("click", function () {
@@ -124,7 +154,23 @@ $(".mybutton").on("click", function () {
 
     var answerValue = $(this).attr("value");
     trivia.checkAnswer(answerValue);
+    
    
-   setTimeout(goBackToQuestion,3000);
+    setTimeout(goBackToQuestion,3000);
    
 })
+
+var startAgain=function(){
+    trivia.index = 0; 
+    trivia.correct=0;
+    trivia.incorrect=0;
+    trivia.unAnswered=0;
+    trivia.isCorrect=false;
+    $(".myRow3").addClass("invisible");
+    $(".myRow3").addClass("tempwidth");
+    $(".myRow2").removeClass("invisible");
+    trivia.fillQuestionAndAnswer();
+    myInterval = setInterval(displayTimer, 1000);
+}
+ // Adding click event listeners to the dynamically created button of class  "startAgain"
+ $(document).on("click", ".startAgain", startAgain);
